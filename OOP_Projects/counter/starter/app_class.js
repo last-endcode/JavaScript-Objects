2b1b92c9666052f3515a95d2b85d01cf00ea9acf
Wira Wardhana <wiwa.linuxgeek@gmail.com>
2021-01-04 14:36:48 +0700

    update counter

--- OOP_Projects/counter/starter/app_class.js
+++ OOP_Projects/counter/starter/app_class.js
@@ -1,53 +1,47 @@
-// getElement for select items
 function getElement(selection) {
   const element = document.querySelector(selection);
-  // if element exist return element
-  // if not get message error
   if (element) {
     return element;
   }
-  throw new Error(
-    `please checked again cause this element ${selection} not exist`
-  );
+  throw new Error(`Please check again your element ${selection} not found`);
 }
 
+// use class
 class Counter {
   constructor(element, value) {
-    this.counter = element;
+    this.element = element;
     this.value = value;
-    // #2 now you can select btn here,
-    // krna kita sudah membuat contoh instance new Counter
-    this.decreaseBtn = element.querySelector('.decrease');
+    // console.log(this.value); 100 / 200
+    // console.log(this.element); // result element first-container and secondcontainer
     this.resetBtn = element.querySelector('.reset');
     this.increaseBtn = element.querySelector('.increase');
-    // console.log(this.resetBtn);
-    // check will output 2 element btn reset sesuai dgn
-    // 2 object yang dibuat <button class="btn reset">reset</button>
-    // krna cek log element hasilnya adalah .first-container & .second-container
+    this.decreaseBtn = element.querySelector('.decrease');
     this.valueDOM = element.querySelector('.value');
     this.valueDOM.textContent = this.value;
-    // bind this to all function
+    // bind to action
     this.increase = this.increase.bind(this);
     this.decrease = this.decrease.bind(this);
     this.reset = this.reset.bind(this);
+
     this.increaseBtn.addEventListener('click', this.increase);
-    this.resetBtn.addEventListener('click', this.reset);
     this.decreaseBtn.addEventListener('click', this.decrease);
+    this.resetBtn.addEventListener('click', this.reset);
   }
-  // function
+  // method here : increase(),decrease(),reset()
   increase() {
-    this.value++; //increment
-    // update
-    this.valueDOM.textContent = this.value;
+    this.value++; //for add +1
+    this.valueDOM.textContent = this.value; //update
   }
   decrease() {
-    this.value--;
-    this.valueDOM.textContent = this.value;
+    this.value--; //for add +1
+    this.valueDOM.textContent = this.value; //update
   }
   reset() {
-    this.value = 0;
-    this.valueDOM.textContent = this.value;
+    this.value = 0; // reset to 0
+    this.valueDOM.textContent = this.value; //update
   }
 }
-const firstCounter = new Counter(getElement('.first-counter'), 100);
-const secondCounter = new Counter(getElement('.second-counter'), 200);
+
+// create object
+const firstCounter = new Counter(getElement('.first-counter'), 10);
+const secondCounter = new Counter(getElement('.second-counter'), 10);
--- OOP_Projects/counter/starter/app_proto.js
+++ OOP_Projects/counter/starter/app_proto.js
@@ -1,56 +1,64 @@
 function getElement(selection) {
   const element = document.querySelector(selection);
+  // ** IF exist return,
+  // ** IF NOT Send ERROR Information
   if (element) {
     return element;
   }
-  throw new error(
-    `please check again for element cause ${selection} not exist`
+  throw new Error(
+    `Please check again element on html ${selection} its not found`
   );
 }
 
-class Counter {
-  constructor(element, nilai) {
-    this.counter = element;
-    this.nilai = nilai;
-    this.resetBtn = element.querySelector('.reset');
-    this.increaseBtn = element.querySelector('.increase');
-    this.decreaseBtn = element.querySelector('.decrease');
-    this.nilaiDOM = element.querySelector('.value');
-    this.nilaiDOM.textContent = this.nilai;
-    // ******  click ******
-    // console.log(this.increase); output:
-    // ƒ increase() {
-    // this.nilai++;
-    // this.nilaiDOM.textContent = this.nilai;
-    // console.log(this); #checked
-    // use bind cz increase include Counter
-    this.increase = this.increase.bind(this);
-    this.decrease = this.decrease.bind(this);
-    this.reset = this.reset.bind(this);
-    this.increaseBtn.addEventListener('click', this.increase);
-    this.decreaseBtn.addEventListener('click', this.decrease);
-    this.resetBtn.addEventListener('click', this.reset);
-  }
-  increase() {
-    this.nilai++;
-    this.nilaiDOM.textContent = this.nilai;
-    // console.log(this); #checked
-  }
-  decrease() {
-    this.nilai--;
-    this.nilaiDOM.textContent = this.nilai;
-    // console.log(this); #checked
-  }
-  reset() {
-    this.nilai = 0;
-    this.nilaiDOM.textContent = this.nilai;
-  }
+function Counter(element, value) {
+  this.element = element;
+  this.value = value;
+  // console.log(this.value); 100 / 200
+  // console.log(this.element); // result element first-container and secondcontainer
+  this.resetBtn = element.querySelector('.reset');
+  this.increaseBtn = element.querySelector('.increase');
+  this.decreaseBtn = element.querySelector('.decrease');
+  this.valueDOM = element.querySelector('.value');
+  this.valueDOM.textContent = this.value;
+  // bind to action
+  this.increase = this.increase.bind(this);
+  this.decrease = this.decrease.bind(this);
+  this.reset = this.reset.bind(this);
+
+  this.increaseBtn.addEventListener('click', this.increase);
+  this.decreaseBtn.addEventListener('click', this.decrease);
+  this.resetBtn.addEventListener('click', this.reset);
 }
 
-const firstCounter = new Counter(getElement('.first-counter'), 10);
-const secondCounter = new Counter(getElement('.second-counter'), 10);
+// *** add prototype for:
+// *** increase, decrease, reset
+Counter.prototype.increase = function () {
+  console.log(this);
+  this.value++; //for add +1
+  this.valueDOM.textContent = this.value; //update
+};
 
-// here will checked
-// bahwa niali 10 akan bertmbah 1
-firstCounter.increase();
-firstCounter.decrease();
+Counter.prototype.decrease = function () {
+  this.value--; //for add -1
+  this.valueDOM.textContent = this.value; //update
+};
+
+Counter.prototype.reset = function () {
+  this.value = 0;
+  this.valueDOM.textContent = this.value; //update
+};
+
+// ingat! pembuatan obj sesuaikan dgn isi param func counter
+// disini ada 2 param element dan value
+const firstCounter = new Counter(getElement('.first-counter'), 100);
+const secondCounter = new Counter(getElement('.second-counter'), 200);
+
+// example just for check
+// firstCounter.reset();
+
+/* NOTE 
+untuk penggunaan btn , value
+menggunakan element.querySelector karena
+element2 tsb termasuk kedalam element yang dibuat 
+dalam 2 object tsb .first-counter, & .second-counter
+*/
